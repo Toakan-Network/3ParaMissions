694c325a4aabe15167c1685e49d45589b7862570
Toakan <7566941+Toakan@users.noreply.github.com>
2023-06-16 22:00:38 +0100

    Setup Fortify tool basics
    Signed-off-by: Toakan <7566941+Toakan@users.noreply.github.com>

---
+++ operation/includes/fortify.hpp
@@ -0,0 +1,17 @@
+class ACEX_Fortify_Presets {
+	class PARA_Normal {
+        displayName = "3Para Fortify";
+        objects[] = {
+            {"Sandbag", 5},
+            {"Bunker", 50}
+        };
+    };
+    class TAG_categories {
+        displayName = "Foritifications";
+        objects[] = {
+            {"Sandbag", 5, "Light"},
+            {"Bunker", 50, "Bunker"}
+        };
+	};
+};
+
