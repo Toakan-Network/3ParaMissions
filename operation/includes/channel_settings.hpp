33898b2dfc441ab892b94c8ab9fd6c0e484db0e6
Toakan <7566941+Toakan@users.noreply.github.com>
2023-07-05 20:37:25 +0100

    Removed RadioChannels
    Signed-off-by: Toakan <7566941+Toakan@users.noreply.github.com>

---
+++ operation/includes/channel_settings.hpp
@@ -0,0 +1,15 @@
+/*
+	0 = Global
+	1 = Side
+	2 = Command
+	3 = Group
+	4 = Vehicle
+	5 = Direct
+	6 = System
+*/
+disableChannels[]={
+	{0, true, true},
+	{1, true, true},
+	{2, true, true},
+	{6, true, true}
+};
