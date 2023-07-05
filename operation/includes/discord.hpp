620e2dd0f60506cc08b4428df2aeccd454bbe6e5
Toakan <7566941+Toakan@users.noreply.github.com>
2023-07-05 20:37:39 +0100

    Add Discord RichPresence
    Signed-off-by: Toakan <7566941+Toakan@users.noreply.github.com>

---
+++ operation/includes/discord.hpp
@@ -0,0 +1,11 @@
+class CfgDiscordRichPresence {
+    applicationID="1125443253840388167";             // Provided by discord
+    defaultDetails="3PARA | ARMA Servers";            // Upper text
+    defaultState="Currently in game.";              // Lower text  
+    defaultLargeImageKey="3para";      // Large image
+    defaultLargeImageText="3 PARA";     // Large image hover text
+    defaultSmallImageKey="armalogo";      // Small image
+    defaultSmallImageText="ARMA 3";     // Small image hover text
+    defaultButtons[]={};          // Button texts and urls
+    useTimeElapsed=1;             // Show time elapsed since the player connected (1 - true, 0 - false)
+};
